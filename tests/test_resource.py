c59ed32201ff03e71c23736cc0d5611845a57a36
Hadrien David <hadrien@ectobal.com>
2020-02-02 18:50:29 -0500

    chore: generate project skeleton (#5)

---
+++ tests/test_resource.py
@@ -0,0 +1,8 @@
+from pytest import raises
+
+
+def test_pattern_child_fails_when_no_pattern_child_exists():
+    from mulatu import Resource
+
+    with raises(StopIteration):
+        Resource(None, "/", None).pattern_child
